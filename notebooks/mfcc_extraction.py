a60cf1b6f6a2fc8f8a52006e4a2dbe9020b44dd8
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-01 19:27:03 -0800

    Add MFCC extraction helper

---
+++ notebooks/mfcc_extraction.py
@@ -0,0 +1,13 @@
+import numpy as np
+import librosa
+
+train_files = '../../data/ASVspoof_Dataset/ASVspoof2019_LA_train/flac/'
+
+def extract_mfcc(file_name):
+    file_path =  train_files + file_name + ".flac"
+    
+    y, sr = librosa.load(file_path, sr=16000)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    m = np.mean(mfcc, axis=1)
+    std = np.std(mfcc, axis=1)
+    return np.concatenate([m, std])
