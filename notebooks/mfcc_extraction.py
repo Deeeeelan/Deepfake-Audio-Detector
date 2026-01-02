380a0633f8431520b4f827fc5b1df78084300fae
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-01 20:39:08 -0800

    Double MFCC features extracted

--- notebooks/mfcc_extraction.py
+++ notebooks/mfcc_extraction.py
@@ -7,7 +7,7 @@ def extract_mfcc(file_name):
     file_path =  train_files + file_name + ".flac"
     
     y, sr = librosa.load(file_path, sr=16000)
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=25)
     m = np.mean(mfcc, axis=1)
     std = np.std(mfcc, axis=1)
     return np.concatenate([m, std])
