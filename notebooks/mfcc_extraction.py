4a1f9102de7e18b5b78b9bd965d2bc9ac9477710
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-01 21:07:02 -0800

    Fix mfcc extraction extracting wrong data oops

--- notebooks/mfcc_extraction.py
+++ notebooks/mfcc_extraction.py
@@ -1,10 +1,8 @@
 import numpy as np
 import librosa
 
-train_files = '../../data/ASVspoof_Dataset/ASVspoof2019_LA_train/flac/'
-
-def extract_mfcc(file_name):
-    file_path =  train_files + file_name + ".flac"
+def extract_mfcc(file_path):
+    file_path =  file_path
     
     y, sr = librosa.load(file_path, sr=16000)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=25)
