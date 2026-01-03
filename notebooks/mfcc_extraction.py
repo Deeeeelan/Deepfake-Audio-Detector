fc990c39167dc4a6a6a1663373ca1e6e696d5a1c
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-03 12:31:09 -0800

    Update nn to use FoR dataset

--- notebooks/mfcc_extraction.py
+++ notebooks/mfcc_extraction.py
@@ -16,12 +16,9 @@ def extract_mfcc(file_path):
 
 def make_spectrogram(path):
 
-    y, sr = sf.read(path, dtype ='float32')
-    # y, sr = librosa.load(path, sr=None)
-    if sr != target_sr:
-        y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
-        sr = target_sr
-        
+    # y, sr = sf.read(path, dtype ='float32') # NOTE: using sf consumes high memory for the FoR dataset
+    y, sr = librosa.load(path, sr=target_sr)
+
     size = sr * 3 # 3 seconds
     y_fixed = librosa.util.fix_length(y, size=size)
     y_norm = librosa.util.normalize(y_fixed) # normalize audio to an amplitude of 1
