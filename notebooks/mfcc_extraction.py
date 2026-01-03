e1b9aa0c132374c98bc9d10e1b7b334ca0b17374
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-01-03 01:01:36 -0800

    Add a convolutional neural network model and change data location Will get the second dataset working eventually

--- notebooks/mfcc_extraction.py
+++ notebooks/mfcc_extraction.py
@@ -1,9 +1,36 @@
 import numpy as np
 import librosa
+import soundfile as sf
+
+target_sr = 16000
 
 def extract_mfcc(file_path):    
-    y, sr = librosa.load(file_path, sr=16000)
+    y, sr = sf.read(file_path, dtype ='float32')
+    if sr != target_sr:
+        y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
+        sr = target_sr
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=25)
     m = np.mean(mfcc, axis=1)
     std = np.std(mfcc, axis=1)
     return np.concatenate([m, std])
+
+def make_spectrogram(path):
+
+    y, sr = sf.read(path, dtype ='float32')
+    # y, sr = librosa.load(path, sr=None)
+    if sr != target_sr:
+        y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
+        sr = target_sr
+        
+    size = sr * 3 # 3 seconds
+    y_fixed = librosa.util.fix_length(y, size=size)
+    y_norm = librosa.util.normalize(y_fixed) # normalize audio to an amplitude of 1
+
+    mel_spectrogram = librosa.feature.melspectrogram(y=y_norm, 
+                                                    sr=sr,
+                                                    n_fft=2048, 
+                                                    hop_length=512,
+                                                    n_mels=128)
+    log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
+
+    return log_mel_spectrogram
